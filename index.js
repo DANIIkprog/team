cebce3833a7da1080222e8f365d583d1ce2095fe
DANIIkprog <72724550+DANIIkprog@users.noreply.github.com>
2020-12-22 19:00:22 +0900

    this is the beginning ..

---
+++ index.js
@@ -0,0 +1,13 @@
+//objects
+var cvs = document.getElementById('cnv');;// канвас
+var ctx=cvs.getContext("2d"); //игра в 2д
+
+var char= new Image();
+var bg= new Image();
+var ice= new Image();
+
+char.src="img/char.jpg";
+bg.src="img/phon.jpg";
+ice.src="img/ice.png";
+
+
