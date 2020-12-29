bbfce829e028ef7a95c6bdedad118daec812f52e
Люба Данилова <73094030+biziland@users.noreply.github.com>
2020-12-29 12:46:16 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -10,30 +10,38 @@ var charLeft= new Image();
 var charRight= new Image();
 var charLeftJump= new Image();
 var charRightJump= new Image();
+let number0 = new Image();
+let number1 = new Image();
+let number2 = new Image();
 //находим объекты
 char.src="img/front.png";
-bg.src="img/phon.jpg";
-ice.src="img/ice.png";
+bg.src="img/phon.png";
 diamond.src="img/diamond.png";
 charLeft.src="img/run-left.png";
 charRight.src="img/run-right.png";
 charLeftJump.src="img/jump-left.png";
 charRightJump.src="img/jump-right.png";
+number0.src="img/0.png";
+number1.src="img/1.png";
+number2.src="img/2.png";
 
 //выполняем функцию после последнего объекта
 bg.onload= draw;
-
+let d=0;
 //расположение
 let charCol=10 , charRow= 480;
 let move="";
 //рисуем объекты
 function draw(){
+	ctx.clearRect(0, 0, cvs.width, cvs.height);
 	ctx.drawImage(bg, 0, 0);
-	ctx.drawImage(ice,700,430);
-	ctx.drawImage(ice,500,330);
-	ctx.drawImage(ice,700,230);
-	ctx.drawImage(ice,200,330);
-	ctx.drawImage(diamond,40,200);
+	if (d==0) {
+		ctx.drawImage(diamond,90,60); 
+		ctx.drawImage(number0,20,20,50,50);
+	}
+	else {
+		ctx.drawImage(number1,20,20,50,50);
+	}
 	switch (move){
 		case "left":
 			ctx.drawImage(charLeft,charCol,charRow);
@@ -51,19 +59,11 @@ function draw(){
 		    ctx.drawImage(char,charCol,charRow);
 	}
 }
-//расположение льдин
-let iceRow = [700,230];
 
 
 // проверяет запрыгнула на льдину или нет
 let iceCheck=function(charCol,charRow,i){
-	if ((charCol+76)<880 && (charCol+76)>700 && (i+200)==518) 
-		return 1;
-	if ((charCol+76)<680 && (charCol+76)>500 && (i+200)==418) 
-		return 1;
-	if ((charCol+76)<880 && (charCol+76)>700 && (i+200)==318) 
-		return 1;
-	if ((charCol+76)<380 && (charCol+76)>200 && (i+200)==418) 
+	if ((i+200)==510 || (i+200)==387 || (i+200)==238) 
 		return 1;
 }
 //движение 
@@ -72,7 +72,7 @@ function moveOnce(event) {
 	//проверяем нажатую клавишу/клик
 	switch (event.code){
 		case "ArrowRight":
-			if (charCol < 751){
+			if (charCol < 751 || charRow==39){ 
 			charCol+=10;
 			move="right";
 			draw();
@@ -86,18 +86,27 @@ function moveOnce(event) {
 			}
 			break;
 		case "ArrowUp":
-			charRow-=200;
-			move="jump";
-			draw();
-			for(let i=charRow; i<480; i++){                //падение после прыжка
-				 setTimeout(moveJump,300);
-				 if(iceCheck(charCol,charRow,i)==1)break;
+			for(let i=charRow; i>(charRow-200); i--){                 //прыгает
+				setTimeout(moveJump1,100);
 	        }
+	        charRow-=200;
+			for(let j=charRow; j<(charRow+200); j++){                //падает
+				setTimeout(moveJump2,100);
+				if(iceCheck(charCol,charRow,j)==1)break;
+	        }
+	        charRow+=200;
 			break;
 }
+		if ((charCol+100)>150 && (charCol+100)<213 && (charRow+100)<182 && (charRow+100)>132) d=1;//console.log('puk');
+
 }
 //гравитация
-function moveJump(){
+function moveJump1(){
+        charRow--;
+		move="jump";
+		draw();
+}
+function moveJump2(){
         charRow++;
 		move="jump";
 		draw();
